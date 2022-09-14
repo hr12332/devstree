a1ceefbd4189b7d388db208824735aedd940a4aa
hr12332 <107110722+hr12332@users.noreply.github.com>
2022-09-14 17:51:27 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,100 @@
+// let day="monday";
+
+// if(day=="monday"){
+//     console.log("today is monday");
+
+// }
+// else if(day=="tuesday")
+// {
+// console.log("today is tuesday");
+// } 
+// else if(day=="wednesday")
+// {
+// console.log("today is wednesday");
+// }    
+// else if(day=="thursday")
+// {
+// console.log("today is thursday");
+// }    
+// else if(day=="friday")
+// {
+// console.log("today is friday");
+// }    
+// else if(day=="saturday")
+// {
+// console.log("today is saturday");
+// }    
+// else 
+// {
+// console.log("default");
+// }       
+
+
+// function myFunc(theArr) {
+//     theArr[0] = 30;
+//   }
+  
+//   const arr = [45];
+  
+//   console.log(arr[0]); // 45
+//   myFunc(arr);
+
+
+
+// function addition(a,b){
+    
+//     return a+b;
+// }
+
+
+// console.log(addition(2,3));
+
+// function factorial(n){
+    
+    
+//     if(n==0 || n==1){
+//          return 1       
+//     }
+//     else{
+//         return n*factorial(n-1)
+//     }
+// }  
+
+// console.log(factorial(3));
+
+// emp={id:102,name:"Shyam Kumar",salary:40000}
+// console.log(emp); 
+
+// var emp=new Object();  
+// emp.id=101;  
+// emp.name="Ravi Malik";  
+// emp.salary=50000;  
+// console.log(emp.id+""+emp.name+""+emp.salary+"");
+
+// let str="hritik";
+// substring=str.slice( 1, 3)
+// console.log(substring);
+
+
+// let string="hritik";
+// str=string.endsWith("k")
+// console.log(str);
+
+// function cube(a,b,c){
+//     return Math.pow(a,b,c)
+// }
+// console.log(cube(1,2,3))
+  
+// var arr = ['C', 'C++', 'Python'];
+
+// arr.forEach(function(fetch) {
+//  console.log(fetch);
+// });
+
+// let abc = "Hello 123-456 WORLD";
+// const reverseNumbers = str => str.replace(/\d+(?:\.\d+)?/g, m => m.split('').reverse().join(''));
+// console.log(reverseNumbers( "Hello 123-456 WORLD"));
+
+let r =123;
+let t=r.split("").reverse().join("");
+console.log(t);
